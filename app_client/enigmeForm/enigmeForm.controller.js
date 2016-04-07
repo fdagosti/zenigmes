70d4942a03f8b9bda64b89ac1615f1d71ff85af8
Francois D'Agostini <francois.dagostini@gmail.com>
2016-04-07 10:45:05 +0200

    added text area options

--- app_client/enigmeForm/enigmeForm.controller.js
+++ app_client/enigmeForm/enigmeForm.controller.js
@@ -11,8 +11,19 @@
     	};
 
     	$scope.tinymceOptions = {
-    		menubar: "",
-    		statusbar: false
+            languages: "fr_FR",
+            style_formats_merge: true,
+            style_formats:[
+                { title: 'Encadré', inline: 'span', styles: { display: 'inline-block', border: '1px solid black', padding: '2px 5px', margin: '0 2px', color: 'black' } },
+            ],
+            plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste imagetools textcolor colorpicker"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | forecolor backcolor",
+            
+  
   		};
 
 

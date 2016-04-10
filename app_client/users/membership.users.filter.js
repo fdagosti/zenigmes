8b001121a15fa507d5e323052c9412552ef66a4a
Francois D'Agostini <francois.dagostini@gmail.com>
2016-04-10 21:56:44 +0200

    added admin check for users

---
+++ app_client/users/membership.users.filter.js
@@ -0,0 +1,14 @@
+(function(){
+
+  angular
+    .module('zenigmesApp')
+    .filter('formatMembership', formatMembership);
+  function formatMembership() {
+    return function (membership) {
+      if (membership === "admin")
+        return "Administrateur";
+      else
+        return "Membre";
+    };
+  };
+})();

491c3c93cfe17d085f520e666998aba4dde1e82d
fdagosti <francois.dagostini@gmail.com>
2016-06-17 22:04:53 +0200

    landing page yields to ranking

--- app_client/src/app/landing/landing.controller.js
+++ app_client/src/app/landing/landing.controller.js
@@ -1,6 +1,6 @@
 
 (function(){
-  angular.module('zenigmesApp').controller('landingCtrl', function($scope, authentication) {
+  angular.module('zenigmesApp').controller('landingCtrl', function($scope, authentication, sessionsData) {
     var vm = this;
 
     vm.isLoggedIn = authentication.isLoggedIn();
@@ -8,6 +8,19 @@
     authentication.subscribe($scope, "landing", function() {
       vm.isLoggedIn = authentication.isLoggedIn();
     });
+
+    
+
+    vm.areButtonEnabled = function(){
+      return vm.participations && vm.participations.length >0;
+    }
+
+    sessionsData.participations().then(function(participations){
+        vm.participations = participations;
+        vm.mostRecentDefi = vm.participations[0]._id;
+      }).then(function(err){
+        vm.error = err;
+      })
 
   });
 })();

f25ce1f3bd061f6b92fb97f0f8071aee576828e2
francois D'Agostini <francois.dagostini@gmail.com>
2016-06-24 16:16:00 +0200

    removed hardcoded credentials

--- app_api/config/mail.js
+++ app_api/config/mail.js
@@ -1,12 +1,11 @@
 var nodemailer = require('nodemailer');
 var mg = require('nodemailer-mailgun-transport');
-var app = require("../../app");
 
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
 var auth = {
   auth: {
-    api_key: 'key-436fca811b3f1643e817060c6e154584',
-    domain: 'sandbox067799b736ae41158183a45305be7cc3.mailgun.org'
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN
     }
 }
 var transport = mg(auth);

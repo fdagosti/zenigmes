1bcdbeb49e13ede8adcc8b90f647635ac716fa72
fdagosti <francois.dagostini@gmail.com>
2016-07-05 00:56:31 +0200

    first mes defis refactoring

--- app_client/src/app/mesDefis/mesDefis.controller.js
+++ app_client/src/app/mesDefis/mesDefis.controller.js
@@ -25,6 +25,61 @@ angular.module('zenigmesApp').controller('mesDefisCtrl', function($scope, sessio
     return vm.getSessionEndDate(session) < new Date(); 
   };
 
+  vm.canAnswerEnigme = function(defi){
+    return defi.enigmeDuMoment != null && !defi.enigmeDuMoment.alreadyAnswered;
+  };
+
+  var s = 1000;
+  var m = 60*s;
+  var h = 60*m;
+  var d = 24*h;
+
+  vm.getEnigmeDuMomentEndTime = function(defi){
+    var enigmeDuMomentId = defi.enigmeDuMoment._id;
+    for (var i = 0; i < defi.enigmes.length; i++) {
+      if (defi.enigmes[i].enigme === enigmeDuMomentId){
+        return defi.enigmes[i].end;
+      }
+    }
+  }
+
+  vm.sortValue = function(session){
+    if (vm.canAnswerEnigme(session)){
+      return 0;
+    }
+    if (session.enigmeDuMoment){
+      return 1;
+    }
+
+    return 2;
+  }
+
+  vm.getTimeToAnswer = function(defi){
+    if (!defi.enigmeDuMoment) {return "";}
+
+    var endTime = vm.getEnigmeDuMomentEndTime(defi);
+    var timeToAnswer = new Date(endTime) - new Date();
+
+    var day = Math.floor(timeToAnswer / d);
+    timeToAnswer = timeToAnswer % d;
+    var hours = Math.floor(timeToAnswer / h);
+    timeToAnswer = timeToAnswer % h;
+    var mins = Math.floor(timeToAnswer / m);
+    timeToAnswer = timeToAnswer % m;
+    var secs = Math.floor(timeToAnswer / s);
+    timeToAnswer = timeToAnswer % s;
+
+    if (day !== 0){
+      return day + " jours, "+hours+" heures et "+mins+" minutes";
+    } else if (hours !== 0){
+      return hours+" heures et "+mins+" minutes";
+    } else if (mins !== 0){
+      return mins+" minutes et "+secs+" secondes";
+    }
+    return secs+" secondes";
+
+  }
+
 });
 
 }))();

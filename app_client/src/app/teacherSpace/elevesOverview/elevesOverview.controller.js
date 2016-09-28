c59885717ba6667949781303699ac405f2740255
fdagosti <francois.dagostini@gmail.com>
2016-09-28 10:20:53 +0200

    improved a bit the classes screen

--- app_client/src/app/teacherSpace/elevesOverview/elevesOverview.controller.js
+++ app_client/src/app/teacherSpace/elevesOverview/elevesOverview.controller.js
@@ -8,6 +8,7 @@
         function listUsers(){
             classes.allClasses().then(function(data){
                 vm.classes = data;
+                _computeTotalStudentNumber();
             },function(error){
                 vm.error = error.statusText;
             });
@@ -35,6 +36,42 @@
             }
             return result;
         }
+
+        _computeTotalStudentNumber = function(){
+            var totalStudents = totalClasses = 0;
+            vm.classes.forEach(function(classe){
+                if (classe.className !== "externe"){
+                    totalClasses += classe.classeNumbers.length;
+                    totalStudents += vm.getStudentsNbPerClass(classe);
+                }
+            });
+            vm.totalStudents = totalStudents;
+            vm.totalClasses = totalClasses;
+        };
+
+        var _classeOrder = {
+            "6eme": 0,
+            "5eme": 1,
+            "4eme": 2,
+            "3eme": 3,
+            "2nde": 4,
+            "1ere": 5,
+            "terminale": 6,
+            "externe": 7,
+        }
+
+        vm.orderByClasse = function(classe){
+            return _classeOrder[classe.className];
+        };
+
+        vm.classForGroup = function(classe){
+            console.log("class for group");
+            if (classe.className === "externe"){
+                return "panel panel-warning";
+            }else{
+                return "panel panel-default";
+            }
+        };
 
        
         listUsers();
